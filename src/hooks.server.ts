effc07ced8168d280796a6f859b506ed04304535
TreltaSev <treltasev@gmail.com>
2025-04-26 11:45:34 -0400

    fix(cookie): Sveltekit switching to relative pathing for cookies

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,6 @@
 import { authenticate } from "$lib/server/authenticate";
 import { redirect, type Handle } from "@sveltejs/kit";
+import { cookie_options } from "$lib/server/utils";
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const is_protected =
@@ -9,8 +10,8 @@ export const handle: Handle = async ({ event, resolve }) => {
 	const auth = authenticate(event.cookies);
 
 	if (is_protected && !auth) {
-		event.cookies.delete("email");
-		event.cookies.delete("name");
+		event.cookies.delete("email", cookie_options);
+		event.cookies.delete("name", cookie_options);
 		throw redirect(307, "/login");
 	}
 
