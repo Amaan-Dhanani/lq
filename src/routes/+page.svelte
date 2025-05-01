d38980fef144deb56f17b8daf98bbdd3a769e907
Amaan Dhanani <mauicodyskye1@gmail.com>
2025-05-01 15:16:23 -0400

    2nd commit from ThinkPad T410 Trelta gave....

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,10 @@
 	import { Input } from '@lib/components';
 	export let form: ActionData;
 </script>
-
-<h1 class="text-[32px] mt-[86px] font-bold ml-[5px] mb-0">Sign Up</h1>
+<h1 class="text-[32px] pt-[86px] font-bold ml-[5px] mb-0">Sign Up</h1>
 <p class="mt-0 ml-[5px] mb-[10px] text-[12px]  text-[#B8B8D2]">Enter your details below to start your journey!</p>
-
 <form method="POST" autocomplete="off" use:enhance class="rounded-t-2xl py-8 border-[#ccc] border-[1px] border-b-[0px] px-6 box-border flex flex-col gap-4">
+	
 	<div>
 
 		<Input type="email" id="email_input" name="email" label="Your Email" value={form?.user?.email ?? ''} />
@@ -25,7 +24,7 @@
 		>Register</button
 	>
 </form>
-<p class="text-center text-[#858597] text-[14px]">Already have an account? <a class="text-[#3D5CFF] font-bold underline">Log in</a></p>
+<p class="text-center text-[#858597] text-[14px]">Already have an account? <a class="text-[#3D5CFF] font-bold underline" href="/login">Log in</a></p>
 
 
 {#if form?.message}
