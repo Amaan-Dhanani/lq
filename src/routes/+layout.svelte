e924a4f14576c2bbca3b8fac32679f402720d679
Amaan Dhanani <mauicodyskye1@gmail.com>
2025-05-30 15:45:53 -0400

    dark mode/light mode OFFICIALLY DONE thx TreltaSev/tour-package repo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,26 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-  import Nav from '$lib/components/Nav.svelte';
-  import RemoveGlobalComponent from '$lib/components/RemoveGlobalComponent.svelte';
-  import { onMount } from 'svelte';
-  import Speedial from '@lib/components/darklight/speedial.svelte';
+	import Nav from '$lib/components/Nav.svelte';
+	import RemoveGlobalComponent from '$lib/components/RemoveGlobalComponent.svelte';
+	import { onMount } from 'svelte';
+	import Speedial from '@lib/components/darklight/speedial.svelte';
+	import { global_mode$ } from '$lib/components/darklight/mode';
+	import { type Writable } from 'svelte/store';
+
+	let mode$: Writable<'dark' | 'light' | string> = global_mode$.mode$;
+
+	onMount(() => {
+		mode$.subscribe((v) => {
+			localStorage.theme = v; // Store current theme dynamically
+			document.documentElement.classList.toggle('dark', v === 'dark');
+		});
+	});
 </script>
 
 
 <main class="size-full">
 	<slot />
-	<Speedial/>
+	<Speedial />
 </main>
 
 <style>
