d8a85105f35e29b65cede55fecfafcb1e135e342
Amaan Dhanani <mauicodyskye1@gmail.com>
2025-05-31 19:42:04 -0400

    register errors/success formatted, login errors pending

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -7,12 +7,13 @@
 </script>
 
 <!-- Full screen container -->
-<div class="fixed inset-0 -z-[100] overflow-hidden w-screen h-[300vh] bg-[#F0F0F2] dark:bg-[#1F1F39] mt-[100vh]"></div>
-<div class="flex flex-col min-h-screen bg-[#F0F0F2] dark:bg-[#1F1F39]">
-
+<div
+	class="fixed inset-0 -z-[100] mt-[100vh] h-[300vh] w-screen overflow-hidden bg-[#F0F0F2] dark:bg-[#1F1F39]"
+></div>
+<div class="flex min-h-screen flex-col bg-[#F0F0F2] dark:bg-[#1F1F39]">
 	<!-- Header -->
-	<h1 class="text-[32px] pt-[86px] font-bold ml-[5px] mb-0 dark:text-white">Sign Up</h1>
-	<p class="mt-0 ml-[5px] mb-[10px] text-[12px] text-[#B8B8D2]">
+	<h1 class="mb-0 ml-[5px] pt-[86px] text-[32px] font-bold dark:text-white">Sign Up</h1>
+	<p class="mt-0 mb-[10px] ml-[5px] text-[12px] text-[#B8B8D2]">
 		Enter your details below to start your journey!
 	</p>
 
@@ -21,37 +22,95 @@
 		method="POST"
 		autocomplete="off"
 		use:enhance
-		class="flex flex-col flex-grow rounded-t-2xl py-8 px-6 box-border gap-4 bg-white dark:bg-[#2F2F42]"
+		class="box-border flex flex-grow flex-col gap-4 rounded-t-2xl bg-white px-6 py-8 dark:bg-[#2F2F42]"
 	>
 		<div>
-			<Input type="text" id="name_input" name="name" label="Name" value={form?.user?.name ?? ''} class="mb-[12px]" />
+			<Input
+				type="text"
+				id="name_input"
+				name="name"
+				label="Name"
+				value={form?.user?.name ?? ''}
+				class="mb-[12px]"
+			/>
 		</div>
 		<div>
-			<Input type="email" id="email_input" name="email" label="Email" value={form?.user?.email ?? ''} />
+			<Input
+				type="email"
+				id="email_input"
+				name="email"
+				label="Email"
+				value={form?.user?.email ?? ''}
+			/>
 		</div>
 		<div>
 			<Input type="password" id="password_input" label="Password" name="password" />
 		</div>
-		
 
 		<!-- Submit Button -->
-		<button class="font-poppins h-12 w-full rounded-xl bg-[#3d5cff] text-center text-white hover:cursor-pointer">
+		<button
+			class="font-poppins h-12 w-full rounded-xl bg-[#3d5cff] text-center text-white hover:cursor-pointer"
+		>
 			Register
 		</button>
 
 		<!-- Login Link -->
-		<p class="text-center text-[#858597] text-[14px]">
-			Already have an account? <a class="text-[#3D5CFF] font-bold underline" href="/login">Log in</a>
+		<p class="text-center text-[14px] text-[#858597]">
+			Already have an account? <a class="font-bold text-[#3D5CFF] underline" href="/login">Log in</a
+			>
 		</p>
-
-
-		
-		<!-- Optional messages -->
-		{#if form?.message}
-			<p class="text-[lightgreen]">{form.message}</p>
-		{/if}
-		{#if form?.error}
-			<p class="text-[lightcoral]">{form.error}</p>
-		{/if}
+		<div class="flex flex-col text-center justify-center items-center">
+			<!-- Optional messages -->
+			{#if form?.message}
+				<div
+					id="toast-success"
+					class="mb-4 flex w-full max-w-xs items-center rounded-lg bg-[#F0F0F2] p-4 text-gray-500 shadow-sm dark:bg-gray-800 dark:text-gray-400"
+					role="alert"
+				>
+					<div
+						class="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-green-100 text-green-500 dark:bg-green-800 dark:text-green-200"
+					>
+						<svg
+							class="h-5 w-5"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="currentColor"
+							viewBox="0 0 20 20"
+						>
+							<path
+								d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"
+							/>
+						</svg>
+						<span class="sr-only">Check icon</span>
+					</div>
+					<div class="font-poppins ms-3 text-sm">{form.message}</div>
+				</div>
+			{/if}
+			{#if form?.error}
+				<div
+					id="toast-danger"
+					class="mb-4 flex w-full max-w-xs items-center rounded-lg bg-[#F0F0F2] p-4 text-gray-500 shadow-sm dark:bg-gray-800 dark:text-gray-400"
+					role="alert"
+				>
+					<div
+						class="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-red-100 text-red-500 dark:bg-red-800 dark:text-red-200"
+					>
+						<svg
+							class="h-5 w-5"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="currentColor"
+							viewBox="0 0 20 20"
+						>
+							<path
+								d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z"
+							/>
+						</svg>
+						<span class="sr-only">Error icon</span>
+					</div>
+					<div class="ms-3 text-sm font-normal">{form.error}</div>
+				</div>
+			{/if}
+		</div>
 	</form>
 </div>
