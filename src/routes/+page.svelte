56d22a0858b3a12fa829344d6cf0e3cb9eb248d7
Amaan Dhanani <mauicodyskye1@gmail.com>
2025-06-02 16:10:08 -0400

    changed loading to success message - stop doing that because i want to start nav

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,53 +1,6 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
-	import type { ActionData } from './$types';
 	import '$lib/css/app.css';
-	import { Input } from '@lib/components';
-	export let form: ActionData;
 </script>
+<div class="w-full">
 
-<!-- Full screen container -->
-<div class="fixed inset-0 -z-[100] overflow-hidden w-screen h-[300vh] bg-[#F0F0F2] dark:bg-[#1F1F39] mt-[100vh]"></div>
-<div class="flex flex-col min-h-screen bg-[#F0F0F2] dark:bg-[#1F1F39]">
-
-	<!-- Header -->
-	<h1 class="text-[32px] pt-[86px] font-bold ml-[5px] mb-0 dark:text-white">Sign In</h1>
-	<p class="mt-0 ml-[5px] mb-[10px] text-[12px] text-[#B8B8D2]">
-		Welcome back to LearnQuest!
-	</p>
-
-	<!-- Form Section -->
-	<form
-		method="POST"
-		autocomplete="off"
-		use:enhance
-		class="flex flex-col flex-grow rounded-t-2xl py-8 px-6 box-border gap-4 bg-white dark:bg-[#2F2F42]"
-	>
-		<div>
-			<Input type="email"
-			id="email_input"
-			name="email"
-			label="Email"
-			value={form?.email ?? ""} />
-		</div>
-		<div>
-			<Input type="password" class="mb-[12px]" id="password_input" label="Password" name="password" />
-		</div>
-		
-
-		<!-- Submit Button -->
-		<button class="h-12 w-full rounded-xl bg-[#3d5cff] text-center text-white hover:cursor-pointer">
-			Sign In
-		</button>
-
-		<!-- Sign In Link -->
-		<p class="text-center text-[#858597] text-[14px]">
-			Don't have an account? <a class="text-[#3D5CFF] font-bold underline" href="/login">Get Started</a>
-		</p>
-
-
-		{#if form?.error}
-			<p class="text-[lightcoral]">{form.error}</p>
-		{/if}
-	</form>
 </div>
