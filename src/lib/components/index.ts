4c0290b21a08d4b4505ade980ad9230b654fc5e4
Amaan Dhanani <mauicodyskye1@gmail.com>
2025-06-21 00:12:03 -0400

    made component name capitalized, changed readme

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,4 +1,4 @@
 export { default as Input } from './input/components/input.svelte';
 export { default as CategorySelect } from './category_select/components/CategorySelect.svelte';
-export { default as LightDark } from './darklight/speedial.svelte';
+export { default as LightDark } from './darklight/Speedial.svelte';
 export { default as Textarea } from './textarea/components/Textarea.svelte'
