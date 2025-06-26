1e794a4d62507b8a3e1e6277c6bb1b6674daa710
Amaan Dhanani <mauicodyskye1@gmail.com>
2025-06-26 16:03:49 -0400

    question4

---
+++ src/routes/create_a_quest/question4/+page.svelte
@@ -0,0 +1,111 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import '$lib/css/app.css';
+	import ImageSubmitter from "$lib/components/image_submitter/ImageSubmitter.svelte";
+	export let data;
+	const { tempquest } = data;
+	import { CategorySelect } from '$lib/components';
+	let selected2 = tempquest.correctanswer_4 || 'A';
+	import { Textarea } from '$lib/components';
+</script>
+
+<!-- Full screen container -->
+<div
+	class="fixed inset-0 -z-[100] mt-[100vh] h-[300vh] w-screen overflow-hidden bg-[#4c4c61] dark:bg-[#1F1F39]"
+></div>
+<div class="mr-[1rem] ml-[1rem] flex min-h-screen flex-col bg-[#4c4c61] dark:bg-[#1F1F39]">
+	<!-- Header -->
+	<h1 class="mb-0 ml-[5px] pt-[86px] text-[32px] font-bold text-white">Create a Quest</h1>
+	<p class="mt-0 mb-[10px] ml-[5px] text-[12px] text-[#B8B8D2]">
+		Unknown subheading
+	</p>
+
+	<!-- Form Section -->
+	<form
+		method="POST"
+		autocomplete="off"
+		use:enhance
+		class="box-border flex flex-grow flex-col rounded-t-2xl bg-white px-6 py-8 dark:bg-[#2F2F42]"
+	>
+		<h1 class="mb-[5px] text-center text-[18px] dark:text-white mb-4">Question 4</h1>
+			<h2 class="mb-[5px] text-[16px] dark:text-white">Question</h2>
+			<Textarea
+				id="question_4"
+				name="question_4"
+				required
+				cap={1850}
+				class="mb-[10px]"
+				value={tempquest?.question_4}
+				placeholder="Enter question here. Maximum length 1850 characters."
+			/>
+			<h2 class="mb-[5px] text-[16px] dark:text-white">Answer Choice A</h2>
+			<Textarea
+				id="answerchoicea_4"
+				name="answerchoicea_4"
+				required
+				cap={1850}
+				value={tempquest?.answerchoicea_4}
+				placeholder="Enter Answer Choice A here. Maximum length 1850 characters."
+			/>
+			<h2 class="mb-[5px] text-[16px] dark:text-white">Answer Choice B</h2>
+			<Textarea
+				id="answerchoiceb_4"
+				name="answerchoiceb_4"
+				required
+				cap={1850}
+				value={tempquest?.answerchoiceb_4}
+				placeholder="Enter Answer Choice B here. Maximum length 1850 characters."
+			/>
+			<h2 class="mb-[5px] text-[16px] dark:text-white">Answer Choice C</h2>
+			<Textarea
+				id="answerchoicec_4"
+				name="answerchoicec_4"
+				required
+				cap={1850}
+				value={tempquest?.answerchoicec_4}
+				placeholder="Enter Answer Choice C here. Maximum length 1850 characters."
+			/>
+			<h2 class="mb-[5px] text-[16px] dark:text-white">Answer Choice D</h2>
+			<Textarea
+				id="answerchoiced_4"
+				name="answerchoiced_4"
+				required
+				cap={1850}
+				value={tempquest?.answerchoiced_4}
+				placeholder="Enter Answer Choice D here. Maximum length 1850 characters."
+			/>
+			<h2 class="mb-[5px] text-[16px] dark:text-white">Description (optional)</h2>
+			<Textarea
+				id="description_4"
+				name="description_4"
+				cap={1850}
+				rows={3}
+				value={tempquest?.description_4}
+				placeholder="Optionally explain the reasoning behind the correct answer or provide extra context. Max length - 1850 chars."
+			/>
+			<h2 class="mb-[5px] text-[16px] dark:text-white">Image Attachment (optional)</h2>
+			<ImageSubmitter name="image_4" id="image_4" dataURI={tempquest?.image_4}/>
+
+		<div class="gap-0">
+			<h2 class="mt-[10px] mb-[5px] text-[16px] dark:text-white">Correct Answer</h2>
+				<CategorySelect
+					name="correctanswer_4"
+					bind:selected={selected2}
+					options={[
+						{ label: 'A', value: 'A' },
+						{ label: 'B', value: 'B' },
+						{ label: 'C', value: 'C' },
+						{ label: 'D', value: 'D' },
+					]}
+				/>
+
+		</div>
+		<br>
+		<!-- Submit Button -->
+		<button
+			class="mt-[20px] h-12 w-full rounded-xl bg-[#3d5cff] text-center text-white hover:cursor-pointer"
+		>
+			Continue Quest Creation
+		</button>
+	</form>
+</div>
